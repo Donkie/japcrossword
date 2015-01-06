4ace1fa20a3c605875363398b97c6350fb21500b
Sidneys1 <sidneys1@live.com>
2015-01-06 13:17:15 -0500

    Refactoring

--- JapaneseCrossword/ImageUtilities.cs
+++ JapaneseCrossword/ImageUtilities.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 
-namespace sodukobilder
+namespace JapaneseCrossword
 {
 
     /// <summary>
@@ -14,7 +14,7 @@ namespace sodukobilder
         /// <summary>
         /// A quick lookup for getting image encoders
         /// </summary>
-        private static Dictionary<string, ImageCodecInfo> encoders = null;
+        private static Dictionary<string, ImageCodecInfo> _encoders;
 
         /// <summary>
         /// A quick lookup for getting image encoders
@@ -25,24 +25,23 @@ namespace sodukobilder
             get
             {
                 //if the quick lookup isn't initialised, initialise it
-                if (encoders == null)
+                if (_encoders == null)
                 {
-                    encoders = new Dictionary<string, ImageCodecInfo>();
+                    _encoders = new Dictionary<string, ImageCodecInfo>();
                 }
 
                 //if there are no codecs, try loading them
-                if (encoders.Count == 0)
-                {
-                    //get all the codecs
-                    foreach (ImageCodecInfo codec in ImageCodecInfo.GetImageEncoders())
-                    {
-                        //add each codec to the quick lookup
-                        encoders.Add(codec.MimeType.ToLower(), codec);
-                    }
-                }
+	            if (_encoders.Count != 0) return _encoders;
 
-                //return the lookup
-                return encoders;
+	            //get all the codecs
+	            foreach (var codec in ImageCodecInfo.GetImageEncoders())
+	            {
+		            //add each codec to the quick lookup
+		            _encoders.Add(codec.MimeType.ToLower(), codec);
+	            }
+
+	            //return the lookup
+                return _encoders;
             }
         }
 
@@ -53,15 +52,15 @@ namespace sodukobilder
         /// <param name="width">The width to resize to.</param>
         /// <param name="height">The height to resize to.</param>
         /// <returns>The resized image.</returns>
-        public static System.Drawing.Bitmap ResizeImage(System.Drawing.Image image, int width, int height)
+        public static Bitmap ResizeImage(Image image, int width, int height)
         {
             //a holder for the result
-            Bitmap result = new Bitmap(width, height);
+            var result = new Bitmap(width, height);
             //set the resolutions the same to avoid cropping due to resolution differences
             result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
             //use a graphics object to draw the resized image into the bitmap
-            using (Graphics graphics = Graphics.FromImage(result))
+            using (var graphics = Graphics.FromImage(result))
             {
                 //set the resize quality modes to high quality
                 graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
@@ -75,36 +74,36 @@ namespace sodukobilder
             return result;
         }
 
-        /// <summary> 
-        /// Saves an image as a jpeg image, with the given quality 
-        /// </summary> 
-        /// <param name="path">Path to which the image would be saved.</param> 
-        /// <param name="quality">An integer from 0 to 100, with 100 being the 
-        /// highest quality</param> 
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// An invalid value was entered for image quality.
-        /// </exception>
-        public static void SaveJpeg(string path, Image image, int quality)
+	    /// <summary> 
+	    /// Saves an image as a jpeg image, with the given quality 
+	    /// </summary> 
+	    /// <param name="path">Path to which the image would be saved.</param>
+	    /// <param name="image"></param>
+	    /// <param name="quality">An integer from 0 to 100, with 100 being the 
+	    /// highest quality</param> 
+	    /// <exception cref="ArgumentOutOfRangeException">
+	    /// An invalid value was entered for image quality.
+	    /// </exception>
+	    public static void SaveJpeg(string path, Image image, int quality)
         {
             //ensure the quality is within the correct range
             if ((quality < 0) || (quality > 100))
             {
                 //create the error message
-                string error = string.Format("Jpeg image quality must be between 0 and 100, with 100 being the highest quality.  A value of {0} was specified.", quality);
+                var error = string.Format("Jpeg image quality must be between 0 and 100, with 100 being the highest quality.  A value of {0} was specified.", quality);
                 //throw a helpful exception
                 throw new ArgumentOutOfRangeException(error);
             }
 
             //create an encoder parameter for the image quality
-            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            var qualityParam = new EncoderParameter(Encoder.Quality, quality);
             //get the jpeg codec
-            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+            var jpegCodec = GetEncoderInfo("image/jpeg");
 
             //create a collection of all parameters that we will pass to the encoder
-            EncoderParameters encoderParams = new EncoderParameters(1);
-            //set the quality parameter for the codec
-            encoderParams.Param[0] = qualityParam;
-            //save the image using the codec and the parameters
+		    var encoderParams = new EncoderParameters(1) {Param = new[]{qualityParam}};
+		    //set the quality parameter for the codec
+		    //save the image using the codec and the parameters
             image.Save(path, jpegCodec, encoderParams);
         }
 
@@ -114,7 +113,7 @@ namespace sodukobilder
         public static ImageCodecInfo GetEncoderInfo(string mimeType)
         {
             //do a case insensitive search for the mime type
-            string lookupKey = mimeType.ToLower();
+            var lookupKey = mimeType.ToLower();
 
             //the codec to return, default to null
             ImageCodecInfo foundCodec = null;
---
+++ JapaneseCrossword/OutputViewer.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace JapaneseCrossword
+{
+    /// <summary>
+    /// Interaction logic for OutputViewer.xaml
+    /// </summary>
+    public partial class OutputViewer
+    {
+	    private readonly SaveFileDialog _dlg;
+
+	    public OutputViewer()
+        {
+	        InitializeComponent();
+	        _dlg = new SaveFileDialog {FileName = "Build", DefaultExt = ".png", Filter = "PNG Files|*.png"};
+        }
+
+	    private void Button_Click(object sender, RoutedEventArgs e)
+        {
+		    var res = _dlg.ShowDialog();
+		    if (res != true) return;
+		    var filename = _dlg.FileName;
+		    var imgsrc = (BitmapSource)img.Source;
+                
+		    using(var fs = new FileStream(filename, FileMode.Create))
+		    {
+			    var png = new PngBitmapEncoder();
+			    png.Frames.Add(BitmapFrame.Create(imgsrc));
+			    png.Save(fs);
+		    }
+        }
+    }
+}
